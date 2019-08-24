2600f5b43d12159e3fedf69175b256cfe95a55ff
Andrew <andrewtolt60@gmail.com>
2019-08-24 14:56:18 +0100

    utils.atl: Added [char]

--- atlast-1.2++/nvramrc.h
+++ atlast-1.2++/nvramrc.h
@@ -6,7 +6,7 @@
 #ifndef __ATL_SRC
 #define __ATL_SRC
 
-uint8_t nvramrc[] = ": sifting\n token $sift\n;\n: include\n token $include\n;\n";
+uint8_t nvramrc[] = ": sifting\n token $sift\n;\n: include\n token $include\n;\n: [char] char ; immediate\n";
 
 #endif
 
