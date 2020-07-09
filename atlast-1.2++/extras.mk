0d82d80fe12768724ab7d18cc3551db72143ef56
Andrew Holt <andrewtholt60@gmail.com>
2020-07-09 16:08:09 +0100

    Ooops

--- atlast-1.2++/extras.mk
+++ atlast-1.2++/extras.mk
@@ -1,7 +1,8 @@
 CC=g++
 CFLAGS=-std=c++11 -g -I/usr/local/include -DLIBSER -DATH
 EXTRAS=""
-ATLCONFIG=-DLINUX -DEXTRAS # -DSYSVIPC
+# CPP_EXTRAS includes boost dependent words."
+ATLCONFIG=-DLINUX -DEXTRAS # -DCPP_EXTRAS -DSYSVIPC
 ATLOBJ=linux.o extraFunc.o
 # LIBRARIES=-lmosquitto
 LIBRARIES = -L/usr/local/lib -lser
