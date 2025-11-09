3b9e7200adeb09c44632dbd5387a3aa11b0b406f
Andrew Holt (Chromebook) <andrewtholt60@gmail.com>
2025-11-09 13:12:55 +0000

    Small changes

--- atlast-1.2++/install.sh
+++ atlast-1.2++/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -x
 
+echo "Copy atlast .."
 sudo cp ./atlast /usr/local/bin
+echo ".. strip exexutable .."
 sudo strip /usr/local/bin/atlast
+echo ".. create lib"
 sudo mkdir -p /usr/local/lib/Small
