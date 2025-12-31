aebfb67d3553f3b861d0dab8a38865ee5ea2d5f2
Andrew Holt (Chromebook) <andrewtholt60@gmail.com>
2025-12-31 16:45:54 +0000

    Changed env variable to MASTER

--- atlast-1.2++/tst.sh
+++ atlast-1.2++/tst.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 set -x
 
-if [ -z "$TEST" ]; then
-    echo "Empty"
-elif [ "$TEST" = "atlast" ]; then
-    echo "123 fred !"
-fi
-
+#if [ -z "$TEST" ]; then
+#    echo "Empty"
+#elif [ "$TEST" = "atlast" ]; then
+#    echo "123 fred !"
+case $MASTER in
+    "atlast")
+        echo "123 fred !";;
+    *)
+        echo "Empty";;
+esac
 
