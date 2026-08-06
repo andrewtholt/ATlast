234c49e0f34686cbed3d7ada0423d3d27eb0dedc
Andrew Holt (Chromebook) <andrewtholt60@gmail.com>
2026-08-06 13:44:03 +0100

    Testing.

--- atlast-1.2++/slave.sh
+++ atlast-1.2++/slave.sh
@@ -3,10 +3,15 @@
 set -x
 
 git status . | grep "nothing to commit, working tree clean"
-echo $?
+if [ $? -ne 0 ] ; then
+    echo "Uncommited changes."
+    exit 0
+fi
+
 
 exit
 
 ./build.sh clean && ./build.sh
 
 ./build.sh
+
