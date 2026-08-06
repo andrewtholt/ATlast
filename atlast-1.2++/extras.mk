d4ee773c55782adbc885716217950a8511770a0b
Andrew Holt (Chromebook) <andrewtholt60@gmail.com>
2026-08-06 10:06:14 +0100

    Build mqtt.ml, add in c++ map.

--- atlast-1.2++/extras.mk
+++ atlast-1.2++/extras.mk
@@ -1,11 +1,7 @@
 CC=g++
-# CFLAGS=-std=c++11 -g -I/usr/local/include -DLIBSER -DATH
 CFLAGS=-std=c++11 -g -I/usr/local/include -DATH
 EXTRAS=""
-# CPP_EXTRAS includes boost dependent words."
 ATLCONFIG=-DLINUX -DEXTRAS -DCPP_EXTRAS -DSYSVIPC
-# ATLOBJ=linux.o extraFunc.o
 ATLOBJ=linux.o
-# LIBRARIES=-lmosquitto
 LIBRARIES = -L/usr/local/lib -lser
 
