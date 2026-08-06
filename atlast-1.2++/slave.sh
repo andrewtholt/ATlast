da45e7dfe8c895968d605458d81261c53ea797aa
Andrew Holt (DevTerm) <andrewtholt60@gmail.com>
2026-08-06 14:48:54 +0100

    modded slave.sh

--- atlast-1.2++/slave.sh
+++ atlast-1.2++/slave.sh
@@ -6,6 +6,8 @@ git status . | grep "nothing to commit, working tree clean"
 if [ $? -ne 0 ] ; then
     echo "Uncommited changes."
     exit 0
+else
+    git pull
 fi
 
 ./build.sh clean && ./build.sh
