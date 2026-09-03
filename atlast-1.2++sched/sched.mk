e88a3fa6d8174101ad1b8c8482446a01db477c97
Andrew Holt (Chromebook) <andrewtholt60@gmail.com>
2026-09-03 16:00:32 +0100

    Adjusted build.

--- atlast-1.2++sched/sched.mk
+++ atlast-1.2++sched/sched.mk
@@ -2,5 +2,5 @@ CC=g++
 CFLAGS=-std=c++11 -g -I/usr/local/include -I/home/andrewtholt60/Source/VerySimpleDatabase
 ATLCONFIG=-DLINUX -DALIGNMENT -DEXPORT -DREADONLYSTRINGS -DWORDSUSED -DSCHED -DATH -DSOCKET -DKV
 ATLOBJ=linux.o sched.o
-LIBRARIES=-lkv -L/home/andrewtholt60/Source/VerySimpleDatabase
+LIBRARIES=-lkv # -L/home/andrewtholt60/Source/VerySimpleDatabase
 
